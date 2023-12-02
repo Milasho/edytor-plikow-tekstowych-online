d271a1336d2625f1864a00a30f4fed44503bb40a
Milasho <malakmilosz@gmail.com>
2023-12-02 15:31:18 +0100

    minor changes to html files

--- root/app.py
+++ root/app.py
@@ -1,3 +1,6 @@
+# ------- Biblioteki -------
+# __________________________
+
 # Zewnetrzne biblioteki
 from flask import Flask, flash, render_template
 import os
@@ -6,6 +9,8 @@ from dotenv import load_dotenv
 # Wewnetrzne biblioteki
 from Pages.menu import Menu
 
+# ------- Konfiguracja -------
+# ____________________________
 
 app = Flask(__name__)   # Deklaracja aplikacji Flask
 load_dotenv()   # Wczytanie zmiennych srodowiskowych z .env
@@ -20,12 +25,14 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/main_menu')
-def main_menu():
+@app.route('/panel')
+def panel():
     menu = Menu()
     menu.princik()
     return '<h1>aaaa</h1>'
 
+# ------- Pozostale -------
+# _________________________
 
 # Kompatybilnosc z uruchomieniem przez Python
 if __name__ == '__main__':
